be3d120d10a2b9f70f420ebdae8f25588bb706d6
Eliezar.Gongora <eliezar.gongora@gmail.com>
2024-11-24 01:37:56 -0700

    wo3

---
+++ scripts/place.js
@@ -0,0 +1,18 @@
+let temp = document.getElementById("#temp");
+let windspeed = document.getElementById("#windspeed");
+let windchill = document.getElementById("#windchill");
+let tempvar = 22.22;
+let windspeedvar = 19;
+
+temp.innerHTML = `${tempvar.toFixed(1)} °C`;
+windspeed.innerHTML = `${windspeedvar.toFixed(1)} km/h`;
+
+if (tempvar <= 10 && windspeedvar > 4.8) {
+    windchill.innerHTML = calculateWindChill(tempvar, windspeedvar).toFixed(1) + "°C";
+} else {
+    windchill.innerHTML = "N/A";
+}
+
+function calculateWindChill(temp, windspeed) {
+    return 13.12 + (0.6215 * temp) - (11.37 * windspeed ** 0.16) + (0.3965 * temp * windspeed ** 0.16);
+}
