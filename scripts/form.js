413e1e10c48ff55107eb773fc5fd7fdea6bbec4e
EliezarGongora <eliezar.gongora@gmail.com>
2024-12-09 15:50:20 -0700

    w05

---
+++ scripts/form.js
@@ -0,0 +1,52 @@
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+const productsname = document.getElementById('productName');
+
+products.forEach(product => {
+  let name = product.name;
+  let id = product.id;
+  let option = document.createElement("option");
+  option.textContent = name;
+  option.value = id;
+  productsname.appendChild(option);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/review.js
@@ -0,0 +1,8 @@
+
+
+const counterElement = document.getElementById('visits')
+
+const visitsString = localStorage.getItem('visitas') || '0'
+const visits = parseInt(visitsString)
+counterElement.innerText = visits + 1
+localStorage.setItem('visitas', visits + 1)
